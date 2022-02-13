66efad4b5f84a24db9bc8fc57d8d2b41a1faa03d
Mr Smith <krygrish@yandex.ru>
2022-02-13 13:49:07 +0300

    Add list to about route, view list in about template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,9 @@
 <h1>О блоге</h1>
 <p>Эксперименты с Laravel</p>
+<p>
+    <ul>
+        @foreach ($team as $member)
+            <li>{{$member['name']}} is {{$member['position']}}</li>
+        @endforeach
+    </ul>
+</p>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,17 @@ Route::get('/', function () {
 });
 
 Route::get('about', function () {
-    return view('about');
+    $tags = ['обучение', 'программирование', 'php', 'oop'];
+    $team = [
+        ['name' => 'John', 'position' => 'manager'],
+        ['name' => 'Jane', 'position' => 'office director'],
+        ['name' => 'Bill', 'position' => 'software engineer'],
+        ['name' => 'Jessy', 'position' => 'QA tester'],
+        ['name' => 'Tom', 'position' => 'lawyer']
+    ];
+    return view('about', ['team' => $team]);
+    //return view('about', ['tags' => $tags]);
+    //return view('about');
 });
 
 Route::get('articles', function () {
