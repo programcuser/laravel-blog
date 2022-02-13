b7cc214af0c889f464bcae9f7f2ae25e26bd5ee7
Mr Smith <krygrish@yandex.ru>
2022-02-13 14:12:32 +0300

    Add layout for about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,12 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel</p>
-<p>
-    <ul>
-        @foreach ($team as $member)
-            <li>{{$member['name']}} is {{$member['position']}}</li>
-        @endforeach
-    </ul>
-</p>
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel</p>
+@endsection
